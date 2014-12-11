b13e8a33737527ade00e454dd89a4b72cbf01422
beak <k.hashikuchi@gmail.com>
2014-12-11 09:08:11 +0900

    add go back web page function to android device

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -3,3 +3,10 @@ var url = args.url || Alloy.Globals.config.baseurl;
 var loginId = args.loginId || '';
 var mainWebView = $.mainTab.getView('mainWebView');
 mainWebView.url = url;
+
+// configurataion of the back button of android
+$.mainTabGroup.addEventListener("androidback", function(){
+	if($.main.getActive()){
+		mainWebView.goBack();
+	}
+});
