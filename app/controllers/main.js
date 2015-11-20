9b70da4f41b57a4112c912e981051fa845565d12
pythonissam <k0uta120u@icloud.com>
2015-11-20 18:25:01 +0900

    The first commit

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -5,45 +5,43 @@ var mainWebView = $.mainTab.getView('mainWebView');
 mainWebView.url = url;
 
 // configurataion of the back button of android
-$.mainTabGroup.addEventListener("androidback", function(){
-	if($.main.getActive()){
-		mainWebView.goBack();
-	}
+$.mainTabGroup.addEventListener("androidback", function () {
+    if ($.main.getActive()) {
+        mainWebView.goBack();
+    }
 });
 
 var getUserLoginInfoClient = Ti.Network.createHTTPClient({
-	onload: function(e){
-		var userLoginInfo = JSON.parse(this.responseText);
-		$.mainTabGroup.title += userLoginInfo.data.display_name;
-	},
-	onerror: function(e){
-			Ti.API.debug(e.error);
-			var errorDialog = Alloy.Globals.getConnectionErrorDialog();
-			errorDialog.show();
-		},
-		timeout: 5000
+    onload: function (e) {
+        var userLoginInfo = JSON.parse(this.responseText);
+        $.mainTabGroup.title += userLoginInfo.data.display_name;
+    },
+    onerror: function (e) {
+        Ti.API.debug(e.error);
+        var errorDialog = Alloy.Globals.getConnectionErrorDialog();
+        errorDialog.show();
+    },
+    timeout: 5000
 });
 getUserLoginInfoClient = Alloy.Globals.addCookieValueToHTTPClient(getUserLoginInfoClient);
 getUserLoginInfoClient.open('POST', Alloy.Globals.config.baseurl + '/wp-admin/admin-ajax.php');
 getUserLoginInfoClient.send({
-	'action': 'get_login_user_info'
+    'action': 'get_login_user_info'
 });
 
-
-
 // configuration of reload function for android
-function reloadWindowAndroid(){
-	if($.main.getActive()){
-		var url = mainWebView.getUrl();
-		mainWebView.setUrl(url);
-	}
+function reloadWindowAndroid() {
+    if ($.main.getActive()) {
+        var url = mainWebView.getUrl();
+        mainWebView.setUrl(url);
+    }
 }
 
-function openTroubleshootingWindow(){
-	var troubleshootingWindow = Alloy.createController('troubleshooting').getView();
-	troubleshootingWindow.open({
-		modal: true,
-	    modalTransitionStyle: Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
-	    modalStyle: Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET
-	});
+function openTroubleshootingWindow() {
+    var troubleshootingWindow = Alloy.createController('troubleshooting').getView();
+    troubleshootingWindow.open({
+        modal: true,
+        modalTransitionStyle: Ti.UI.iPhone.MODAL_TRANSITION_STYLE_COVER_VERTICAL,
+        modalStyle: Ti.UI.iPhone.MODAL_PRESENTATION_FORMSHEET
+    });
 }
