9e0a51f64339eae18cba7f3abd9e81a142c9cd13
pythonissam <k0uta120u@icloud.com>
2016-04-14 15:16:03 +0900

    backup commint Ajax doesn't guarantee the order of requests, so thumbnails and book titles don't coincide. I'm gonna fix it.

--- app/controllers/searchBooks.js
+++ app/controllers/searchBooks.js
@@ -14,21 +14,20 @@ glassIcon.addEventListener("touchend", function(e){
  
 $.mainWin.add(Alloy.Globals.createCommonHeader([glassIcon]));
 
+Ti.API.info(data)
 var parsedData = JSON.parse(data);
-Ti.API.info("the number of items = " + parsedData.length);
+Ti.API.info(parsedData);
 
-var imgURL = new Array(parsedData.length);
+var imgURL = new Array(parsedData.length); // thumbnail urls
+var j = 0;
 for (var i = 0; i < parsedData.length; i++)
 {
-    Ti.API.info("i = " + i);
     var giveMeImgURLClient = Ti.Network.createHTTPClient();
 
-    Ti.API.info("2. i = " + i);
+    // can't guarantee the order...
     giveMeImgURLClient.onload
 	=  function(e)
     {
-	i -= 1;
-	
 	// urls of book images
 	var res = this.responseText;
 	Ti.API.info("res = " + res);
@@ -40,14 +39,15 @@ for (var i = 0; i < parsedData.length; i++)
 	}
 	else
 	{
-	    imgURL[i] = res;
+	    imgURL[j] = res;
 	}
-	Ti.API.info("i = " + i);
-	if (i == 0) // if the last element comes
+	
+	if (j == parsedData.length - 1) // if the last element comes
 	{
 	    Ti.API.info("the last element!");
 	    listBooks(imgURL);
 	}
+	j += 1;
     };
 
     giveMeImgURLClient.onerror
@@ -57,7 +57,7 @@ for (var i = 0; i < parsedData.length; i++)
 	Ti.API.debug(e.error);
     };
 
-
+    Ti.API.info("parsedData[" + i + "].ID = " + parsedData[i].ID);
     giveMeImgURLClient.open('POST', Alloy.Globals.ajaxUrl);
     giveMeImgURLClient.send({
 	action: "echo_thumbnail_url",
@@ -65,31 +65,61 @@ for (var i = 0; i < parsedData.length; i++)
     });
 }
 
+// the display width comes in pixel, so convert it to dpi
+function pixelsToDPUnits(pixel)
+{
+    return (pixel / (Titanium.Platform.displayCaps.dpi / 160));
+}
+
 function listBooks(iu)
 {
-    // the display width comes in pixel, so convert it to dpi
-    function pixelsToDPUnits(pixel)
-    {
-	return (pixel / (Titanium.Platform.displayCaps.dpi / 160));
-    }
     var screenWidthActual = pixelsToDPUnits(Ti.Platform.displayCaps.platformWidth);
+    var screenHeightActual = pixelsToDPUnits(Ti.Platform.displayCaps.platformHeight);
 
-    Ti.API.info("iu[0] = " + iu[0]);
-    var testImg = Ti.UI.createImageView({
-	image: iu[0],
-    });
+    var properTop = screenHeightActual / 4 - 50;
+    
+    for (var i = 0; i < iu.length; i++)
+    {
+	Ti.API.info(parsedData[i].post_title);
+	if (i % 2 == 0)
+	{
+	    var thumbnail = Ti.UI.createImageView({
+		image: iu[i],
+		width: '110px',
+		height: '160px',
+		left: '20%',
+		top: properTop,
+	    });
+	    var title = Ti.UI.createLabel({
+		color: '#00A935',
+		width: '40%',
+		height: '30%',
+		left: '10%',
+		top: properTop + 50,
+		font: { fontsize: 5, fontFamily: 'Helvetica Neue' },
+		text: parsedData[i].post_title,
+	    });
+	}
+	else
+	{
+	    var thumbnail = Ti.UI.createImageView({
+		image: iu[i],
+		right: '20%',
+		top: properTop,
+	    });
+	    var title = Ti.UI.createLabel({
+		color: '#00A935',
+		width: '40%',
+		height: '30%',
+		left: screenWidthActual * 3 / 4 - 55,
+		top: properTop + 50,
+		font: { fontsize: 5, fontFamily: 'Helvetica Neue' },
+		text: parsedData[i].post_title,
+	    });
+	    properTop += screenHeightActual / 3;
+	}
 
-    var testLabel = Ti.UI.createLabel({
-	backgroundColor: '#f00',
-	color: '#999',
-	text: 'TOEIC(R) \u30c6\u30b9\u30c8 BEYOND 990 \u8d85\u4e0a\u7d1a\u30ea\u30fc\u30c7\u30a3\u30f3\u30b0 7\u3064\u306e\u30b3\u30a2\u30b9\u30ad\u30eb',
-	font: { fontsize: 20, fontFamily: 'Helvetica Neue'},
-	textAlign: 'center',
-	width: '40%',
-	height: '100px',
-	left: '10%',
-    });
-
-    $.mainWin.add(testLabel);
-    $.mainWin.add(testImg);
+	$.mainWin.add(thumbnail);
+	$.mainWin.add(title);
+    }
 }
